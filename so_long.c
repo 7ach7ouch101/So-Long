50fe3e25f5a380a4b2a66c8a8cf45cdf43834a48
7ach7ouch101 <69723977+7ach7ouch101@users.noreply.github.com>
2022-06-23 14:49:13 +0100

    Add files via upload

--- so_long.c
+++ so_long.c
@@ -2,18 +2,51 @@
 #include <stdio.h>
 #include <unistd.h> 
 #include <string.h>
+#include<fcntl.h>
+#include <stdlib.h>
+char	*get_next_line(int fd)
+{
+	char	str[8000000];
+	int		i;
+	char	buff[1];
+	int		byte;
+
+	byte = read(fd, buff, 1);
+	i = 0;
+	while (byte > 0)
+	{
+		str[i++] = buff[0];
+		if (buff[0] == '\n')
+			break ;
+		byte = read(fd, buff, 1);
+	}
+	str[i] = '\0';
+	if (str[0] == '\0')
+		return (0);
+	return (strdup(str));
+}
 
 int	parse_map(char *map)
 {
-	int i;
 	int fd;
+	char *test;
+	int i;
+	int j;
+
+	j = 0;
 	i = 0;
 	while(map[i] != '.')
 		i++;
 	if(strcmp(&map[i], ".ber") != 0)
 		return (0);
-	fd = open(, O_RDWR)
-	return 1;
+	fd = open(map, O_RDWR);
+	test = get_next_line(fd);
+	while(test)
+	{
+		free(test);
+		test = get_next_line(fd);
+	}
+	return (1);
 }
 
 int	main(int ac, char **av)
@@ -25,7 +58,6 @@ int	main(int ac, char **av)
 			write(1, "Error\n", 6);
 			return 0;
 		}
-		printf("done");
 	}
 	else
 		write(1, "arguments error \n", 17);
